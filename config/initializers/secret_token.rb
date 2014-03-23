77c07d134411f8815d6e65d66213723e6ed399c2
Hamish Gardiner <hamishgardiner@gmail.com>
2014-03-23 11:05:16 +0000

    init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Reprobus::Application.config.secret_key_base = 'e31f08a068b97de747c18b96810785e5d3b8c78d4453b851d15d3e9e5020fee0c80524dcd66645c5a14b0f6dcc227992f64c791d4dfe4c6ab65f8da9ab68dc8d'
