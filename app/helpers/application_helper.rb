20ef1a01e4580d47f562979e569de939b50a4539
Hamish Gardiner <hamishgardiner@gmail.com>
2014-07-20 12:35:02 +0000

    updates to customer

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -16,4 +16,74 @@ module ApplicationHelper
     options = { renderer: RemoteLinkPaginationHelper::LinkRenderer }.merge(options)
     will_paginate(collection, options)
   end  
+   
+  
+  module BootstrapExtension
+    FORM_CONTROL_CLASS = "form-control"
+    LABEL_CONTROL_CLASS = "col-sm-3 control-label"
+
+    # Override the 'text_field_tag' method defined in FormTagHelper[1]
+    #
+    # [1] https://github.com/rails/rails/blob/master/actionview/lib/action_view/helpers/form_tag_helper.rb
+    def text_field(name, value = nil, options = {})
+      class_name = options[:class]
+      if class_name.nil?
+        # Add 'form-control' as the only class if no class was provided
+        options[:class] = FORM_CONTROL_CLASS
+      else
+        # Add ' form-control' to the class if it doesn't already exist
+        options[:class] << " #{FORM_CONTROL_CLASS}" if
+          " #{class_name} ".index(" #{FORM_CONTROL_CLASS} ").nil?
+      end
+
+      # Call the original 'text_field_tag' method to do the real work
+      super
+    end
+    
+    def text_field_tag(name, value = nil, options = {})
+      class_name = options[:class]
+      if class_name.nil?
+        # Add 'form-control' as the only class if no class was provided
+        options[:class] = FORM_CONTROL_CLASS
+      else
+        # Add ' form-control' to the class if it doesn't already exist
+        options[:class] << " #{FORM_CONTROL_CLASS}" if
+          " #{class_name} ".index(" #{FORM_CONTROL_CLASS} ").nil?
+      end
+
+      # Call the original 'text_field_tag' method to do the real work
+      super
+    end
+    
+    def label(object_name, method, content_or_options = nil, options = nil, &block)
+      class_name = options[:class]
+      if class_name.nil?
+        # Add 'form-control' as the only class if no class was provided
+        options[:class] = LABEL_CONTROL_CLASS
+      else
+        # Add ' form-control' to the class if it doesn't already exist
+        options[:class] << " #{LABEL_CONTROL_CLASS}" if
+          " #{class_name} ".index(" #{LABEL_CONTROL_CLASS} ").nil?
+      end
+      # Call the original 'text_field_tag' method to do the real work
+      super
+    end
+    
+    def password_field(object_name, method, options = {})
+      class_name = options[:class]
+      if class_name.nil?
+        # Add 'form-control' as the only class if no class was provided
+        options[:class] = FORM_CONTROL_CLASS
+      else
+        # Add ' form-control' to the class if it doesn't already exist
+        options[:class] << " #{FORM_CONTROL_CLASS}" if
+          " #{class_name} ".index(" #{FORM_CONTROL_CLASS} ").nil?
+      end
+      # Call the original 'text_field_tag' method to do the real work
+      super
+    end    
+  end
+
+  # Add the modified method to ApplicationHelper
+  include BootstrapExtension  
 end
