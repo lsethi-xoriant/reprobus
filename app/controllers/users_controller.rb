5a105a9db577efb7aeceb539ca71f551cb731654
Hamish Gardiner <hamishgardiner@gmail.com>
2014-08-26 08:49:16 +0000

    demo commit

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -26,7 +26,7 @@ class UsersController < ApplicationController
     @user = User.new(user_params)
     if @user.save
 	  sign_in @user
-      user.send_welcome_email
+      @user.send_welcome_email
       flash[:success] = "Welcome to the Super App!"
       redirect_to @user
     else
