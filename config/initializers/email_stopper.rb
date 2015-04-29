bf65cc2c9b6ac656fb136188811fd55d2762bad3
hamishchromebook <hamish@writecode.com.au>
2015-04-29 10:20:25 +1000

    block emails

---
+++ config/initializers/email_stopper.rb
@@ -0,0 +1,23 @@
+class EmailAddressFilter
+  def self.delivering_email(message)
+    message.perform_deliveries = false
+    
+    if message.to == "hamishgardiner@gmail.com" || message.to == "hamishgardiner@hotmail.com" ||
+      message.to == "stephenarnold@gmail.com" || message.to == "tim@eclipsetravel.com.au" ||
+      message.to == "matt@eclipsetravel.com.au"
+      
+      message.perform_deliveries = true
+      return
+    end
+    
+    if message.to.include? "@eclipsetravel.com.au"
+      message.perform_deliveries = true
+      return
+    end
+      
+    # otherwise, the email should NOT be sent
+  end
+end
+
+ActionMailer::Base.register_interceptor(EmailAddressFilter)
+
