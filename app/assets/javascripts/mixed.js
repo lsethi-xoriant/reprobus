a389e1425da8df13f97694f26fe77aa660adef7a
hamishchromebook <hamish@writecode.com.au>
2015-06-01 16:47:25 +1000

    precompile

--- app/assets/javascripts/mixed.js
+++ app/assets/javascripts/mixed.js
@@ -13,7 +13,7 @@ $(document).ready(function() {
   
   $('.datepicker').pickadate({
     selectMonths: true, // Creates a dropdown to control month
-    selectYears: 15 // Creates a dropdown of 15 years to control year
+    selectYears: true // Creates a dropdown of 15 years to control year
   });
         
 });
