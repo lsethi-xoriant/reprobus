cc3558dd671cd8f8e649022a08d4efc10a3fbf0c
Hamish Gardiner <hamishgardiner@gmail.com>
2015-06-25 13:39:20 +0000

    import

--- app/uploaders/import_uploader.rb
+++ app/uploaders/import_uploader.rb
@@ -13,7 +13,8 @@ class ImportUploader < CarrierWave::Uploader::Base
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
   def store_dir
-    "public/uploads/imports/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    #"public/uploads/imports/#{model.class.to_s.underscore}/#{mounted_as}/#{model.id}"
+    "tmp"
   end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
