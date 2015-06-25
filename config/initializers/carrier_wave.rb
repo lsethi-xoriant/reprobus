9ac0181621baa3ec4ba98cce4a6bba9a6ece89ea
Hamish Gardiner <hamishgardiner@gmail.com>
2015-06-25 23:51:13 +0000

    remove aws

--- config/initializers/carrier_wave.rb
+++ config/initializers/carrier_wave.rb
@@ -2,8 +2,8 @@ CarrierWave.configure do |config|
   #config.fog_provider = 'fog/aws'                        # required
   config.fog_credentials = {
     provider:              'AWS',                        # required
-    aws_access_key_id:     'AKIAI23NV4HVM2JSIFPQ',                        # required
-    aws_secret_access_key: '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',                        # required
+    aws_access_key_id:     ENV['AWS_ACCESS_KEY'],                        # required
+    aws_secret_access_key: ENV['AWS_ACCESS_KEY_SECRET'],                        # required
     region:                'ap-southeast-2',                  # optional, defaults to 'us-east-1'
 
   }
