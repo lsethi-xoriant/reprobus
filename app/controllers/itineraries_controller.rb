0c439075b7623eebc2115ea332d2525aa235541a
Hamish Gardiner <hamishgardiner@gmail.com>
2015-11-07 03:20:32 +0000

    pdf tidy

--- app/controllers/itineraries_controller.rb
+++ app/controllers/itineraries_controller.rb
@@ -9,7 +9,10 @@ class ItinerariesController < ApplicationController
 
     respond_to do |format|
       format.pdf do
-        render pdf: "Itinerary_no_" + @itinerary.id.to_s.rjust(8, '0') #,
+        render pdf: "Itinerary_no_" + @itinerary.id.to_s.rjust(8, '0'),
+               show_as_html: params.key?('debug')
+        
+        #,
                #footer:  {   html: {   template:'itineraries/print_itinerary/footer.pdf.erb',
                #                       layout: false
                #}}
