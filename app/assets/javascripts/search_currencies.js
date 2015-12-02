a2fb3645f678728d95f71df02d58235452097c15
Hamish Gardiner <hamishgardiner@gmail.com>
2015-12-02 12:23:18 +0000

    test supplier currencies

--- app/assets/javascripts/search_currencies.js
+++ app/assets/javascripts/search_currencies.js
@@ -49,7 +49,7 @@ $(document).ready(function() {
   $eventSelect.on("select2:select", function(e) {
     var data = e.params.data;
     var sellRateField = $(this).closest('.row').find(".sell_currency_rate");
-    //console.log(data);
+console.log("Hi hamish");
     
     sellRateField.val(data.currency_rate);
   });
