573dd3afba80ff14829b1d8038fc747ecdc07443
Dmitry Masukevich <masukevich@gmail.com>
2015-12-14 15:09:19 +0300

    Atachments added

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151211115545) do
+ActiveRecord::Schema.define(version: 20151211143529) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -92,6 +92,7 @@ ActiveRecord::Schema.define(version: 20151211115545) do
     t.datetime "emailed_at"
     t.string   "emailed_to"
     t.integer  "document_type"
+    t.string   "attachment"
   end
 
   create_table "customers", force: :cascade do |t|
