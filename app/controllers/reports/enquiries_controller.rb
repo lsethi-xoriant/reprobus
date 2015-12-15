d48599f82a89558ba46646805d21af647151fbea
Igor Zvezdin <wendigooor@gmail.com>
2015-12-15 14:41:26 +0300

    Additional search params for enquiry reports

--- app/controllers/reports/enquiries_controller.rb
+++ app/controllers/reports/enquiries_controller.rb
@@ -1,12 +1,21 @@
 class Reports::EnquiriesController < ApplicationController
 
   def index
-    @from, @to = params[:reports_search][:from], params[:reports_search][:to] if params[:reports_search]
+    if params[:reports_search]
+      @from, @to   = params[:reports_search][:from], params[:reports_search][:to]
+      @stage       = params[:reports_search][:stage]
+                      .map {|prm| prm[0] if prm[1] == "1"} ## Warning. This map needed for collect values from checkboxes form
+                      .compact                             ## value == "1" mean checked, value == "0" mean unchecked
+      stage        = @stage.any? ? @stage : Enquiry::STATUSES   ## If there is no values we choose all statuses by default
+      @assigned_to = params[:reports_search][:assigned_to_id]
+    end
     @enquiries =
       if @from && @to
         Enquiry
          .includes(:destination, itineraries: [:itinerary_price])
-         .where(created_at: (@from..@to))
+         .where(created_at: (@from..@to),
+                stage: stage,
+                assigned_to: @assigned_to)
       else
         Enquiry
          .includes(:destination, itineraries: [:itinerary_price])
@@ -17,6 +26,7 @@ class Reports::EnquiriesController < ApplicationController
       format.html
       format.csv do
         attributes  = [ "Id",
+                        "Enquiry name",
                         "Customer name",
                         "Consultant",
                         "Date enquiry received",
@@ -29,6 +39,7 @@ class Reports::EnquiriesController < ApplicationController
           csv << attributes
           @enquiries.each do |enq|
             row = [ enq.id,
+                    enq.name,
                     enq.try(:lead_customer).try(:fullname),
                     enq.try(:assignee).try(:name),
                     enq.created_at.strftime("%d-%m-%Y"),
