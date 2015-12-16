1a6d521e9ea50703f2a0e11231dbb48b00e2eec9
Dmitry Masukevich <masukevich@gmail.com>
2015-12-16 20:32:28 +0300

    Refactoring

--- app/models/itinerary.rb
+++ app/models/itinerary.rb
@@ -59,6 +59,21 @@ class Itinerary < ActiveRecord::Base
 
   has_paper_trail :ignore => [:created_at, :updated_at], :meta => { :customer_names  => :customer_names }
 
+  def self.search(from, to, user=nil, country=nil)
+    results = 
+        includes(:lead_customer, :agent, itinerary_infos: :product)
+        .eager_load(itinerary_infos: :product)
+        .where('? < itineraries.end_date AND ? > itineraries.start_date', from, to)
+
+    results = results.where(user_id: user) if user.present?
+
+    if country.present?
+      results = 
+        results.where('products.country_id = :country_id', country_id: country)
+    end
+    results
+  end
+
   def quote_sent_update_date
     self.update_attribute(:quote_sent, DateTime.now)
   end
