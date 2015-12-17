93870f8f159a07068ae80d31eba59ab22e75832a
Dmitry Masukevich <masukevich@gmail.com>
2015-12-17 16:49:14 +0300

    Ability

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -5,11 +5,11 @@ class Ability
     # Define abilities for the passed in user here. For example:
     #
     #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
+    # if user.admin?
+    #   can :manage, :all
+    # else
+    #   can :read, :all
+    # end
     #
     # The first argument to `can` is the action you are giving the user
     # permission to do.
@@ -28,5 +28,8 @@ class Ability
     #
     # See the wiki for details:
     # https://github.com/CanCanCommunity/cancancan/wiki/Defining-Abilities
+    if user.management?
+      # can [:snapshot], :static_page
+    end
   end
 end
