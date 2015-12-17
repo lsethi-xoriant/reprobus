1128eaa1ef6a551e76069656ab9243a97281f113
Igor Zvezdin <wendigooor@gmail.com>
2015-12-17 14:34:34 +0300

    Update controlles with structures

--- app/services/report_service.rb
+++ app/services/report_service.rb
@@ -14,6 +14,31 @@ class ReportService
     results
   end
 
+  def self.enquiry_search(from, to, stage, assigned_to)
+    search_params = {}
+    search_params[:created_at]  = (from && to) ? (from.to_date.beginning_of_day..to.to_date.end_of_day) : (1.month.ago.beginning_of_day..Date.today.end_of_day)
+    search_params[:stage]       = stage if stage.presence
+    search_params[:assigned_to] = assigned_to if assigned_to.presence
+
+    results = Enquiry
+              .includes(:destination, itineraries: [:itinerary_price])
+              .where(search_params)
+  end
+
+  def self.confirmed_booking(from, to, user=nil)
+    search_params = {}
+    search_params[:booking_confirmed_date] = (from && to) ? (from.to_date.beginning_of_day..to.to_date.end_of_day) : (1.month.ago.beginning_of_day..Date.today.end_of_day)
+    search_params[:booking_confirmed]      = true
+    if user
+      search_params[:itineraries] = {}
+      search_params[:itineraries][:user_id] = user
+    end
+
+    results = ItineraryPrice
+                .includes(:itinerary)
+                .where(search_params)
+  end
+
   def self.generate_csv(entities, titles_methods)
     # titles_methods hash structure:
     # key: name of the field
