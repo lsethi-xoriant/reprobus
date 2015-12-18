db420e63b1be4981db41f5e26869032080548321
Dmitry Masukevich <masukevich@gmail.com>
2015-12-18 18:44:19 +0300

    Admin abilities

--- app/controllers/products/dashboard_controller.rb
+++ app/controllers/products/dashboard_controller.rb
@@ -1,4 +1,6 @@
 class Products::DashboardController < ApplicationController
+  authorize_resource class: Products::DashboardController
+  
   before_filter :signed_in_user
   before_filter :admin_user
   
