fa71665299ad05d934b4a21fb4769ab3471a4fa5
Hamish Gardiner <hamishgardiner@gmail.com>
2016-01-10 10:26:03 +0000

    remove initializer problem

--- config/initializers/pin_payment.rb
+++ config/initializers/pin_payment.rb
@@ -1,5 +1,5 @@
-PinPayment.public_key = Setting.global_settings.pin_payment_public_key
-PinPayment.secret_key = Setting.global_settings.pin_payment_secret_key
+#PinPayment.public_key = Setting.global_settings.pin_payment_public_key
+#PinPayment.secret_key = Setting.global_settings.pin_payment_secret_key
 
 PinPayment.api_url    = 'https://test-api.pin.net.au' # Test endpoint, live is: api.pin.net.au (live)
 
