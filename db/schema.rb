c41a490d83f3aca63000675ff0ad5d3327e155e8
Dmitry Masukevich <masukevich@gmail.com>
2016-01-11 15:47:31 +0300

    Default values

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20151231083859) do
+ActiveRecord::Schema.define(version: 20160111122912) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -339,7 +339,7 @@ ActiveRecord::Schema.define(version: 20151231083859) do
     t.integer  "agent_id"
     t.integer  "lead_customer_id"
     t.date     "end_date"
-    t.integer  "bedding_type"
+    t.integer  "bedding_type",             default: 0
   end
 
   add_index "itineraries", ["customer_id"], name: "index_itineraries_on_customer_id", using: :btree
