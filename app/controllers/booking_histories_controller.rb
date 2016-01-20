324effc28a0ee797a4cb9e0aa6f5286569e2d35f
Dmitry Masukevich <masukevich@gmail.com>
2016-01-20 14:52:07 +0300

    Fix for booking history download

--- app/controllers/booking_histories_controller.rb
+++ app/controllers/booking_histories_controller.rb
@@ -1,6 +1,6 @@
-class BookingHistoryController < ApplicationController
+class BookingHistoriesController < ApplicationController
   before_filter :signed_in_user
-  authorize_resource class: BookingHistoryController
+  authorize_resource class: BookingHistoriesController
   
   def download
     url = BookingHistory.find(params[:id]).try(:attachment).try(:url)
