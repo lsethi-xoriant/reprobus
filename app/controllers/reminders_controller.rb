cc5b1a58d38a7311ddeb71bb3c21728579772f1f
Dmitry Masukevich <masukevich@gmail.com>
2016-01-27 14:18:26 +0300

    Remiders: open enquiries table added

--- app/controllers/reminders_controller.rb
+++ app/controllers/reminders_controller.rb
@@ -2,6 +2,9 @@ class RemindersController < ApplicationController
   authorize_resource class: RemindersController
 
   def index
-    
+    @open_enquiries = Enquiry.all
+    respond_to do |format|
+      format.html
+    end
   end
 end
